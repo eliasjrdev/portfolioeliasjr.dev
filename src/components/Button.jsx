048e6f44e70c92572e747b42fce91777d2c45889
eliasjrdev <eliasjrcosta886@gmail.com>
2025-05-31 19:45:22 -0300

    criação do carrossel de icones de habilidades

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,10 @@ export function Button({text, onClick}){
     return(
         <button
         onClick={onClick} 
-        className="bg-[#1F2226] w-35 p-2 
+        className="bg-[#1F2226] w-full lg:w- p-2 
         text-[#FF014F] cursor-pointer rounded-sm shadow-[6px_9px_13px_0px_rgba(0,_0,_0,_0.1)]
-        hover:bg-gray-600">
+        hover:bg-gray-600 ">
         {text}
         </button>
     );
-}
+}   
